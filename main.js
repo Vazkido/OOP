6e604cf6fb178d4eab56f74f5483cc76c7f65e25
valentine <vazkid02@gmail.com>
2025-01-20 09:25:36 -0500

    example 1

--- main.js
+++ main.js
@@ -1,13 +1,22 @@
-const superHero = {
-  name: "SpiderMAN",
-  costume: "red",
-  power: "Webs",
 
-  introduce() {
-    console.log(
-      `I am ${this.name}, my power is to shoot ${this.power}, i never age!`
-    );
-  },
-};
+// Example 1
 
-console.log(superHero);
+// const superHero = {
+//   name: "SpiderMAN",
+//   costume: "red",
+//   power: "Webs",
+
+//   introduce() {
+//     console.log(
+//       `I am ${this.name}, my power is to shoot ${this.power}, i never age!`
+//     );
+//   },
+// };
+
+// console.log(superHero);
+
+
+
+EXAMPLE 2.
+
+
