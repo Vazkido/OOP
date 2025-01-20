24aca8800e374778c98c3aac8fea04e1b4d8757a
valentine <vazkid02@gmail.com>
2025-01-20 09:24:18 -0500

    initial Commit

---
+++ main.js
@@ -0,0 +1,13 @@
+const superHero = {
+  name: "SpiderMAN",
+  costume: "red",
+  power: "Webs",
+
+  introduce() {
+    console.log(
+      `I am ${this.name}, my power is to shoot ${this.power}, i never age!`
+    );
+  },
+};
+
+console.log(superHero);
