1043630ee6f86a08b3c17c117cb5ff70e27ae896
valentine <vazkid02@gmail.com>
2025-01-20 09:42:14 -0500

    example 2

--- main.js
+++ main.js
@@ -19,4 +19,18 @@
 
 EXAMPLE 2.
 
+function Book(title, author, year) {
+    this.title = title
+    this.author = author
+    this.year = year
+    this.titleLength = title.length
+    this.getSummary = function() {
+        console.log(`The title of this book is ${this.title}, and the author of this book is ${this.author}, in the year ${this.year}.`)
+
+    }
+}
+
+let book1 = new Book("To Kill a Mockingbird", "Harper Lee", 1960)
+
+book1.getSummary(book1)
 
